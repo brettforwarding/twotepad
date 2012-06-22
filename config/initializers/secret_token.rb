868c8bed6d1b09e578ffc3beef9e12193667e2cb
Brett <brettforwarding@gmail.com>
2012-06-22 19:52:33 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twotepad::Application.config.secret_token = '121f5d27da042b969b34b9c72898d6fe99fc2b41d6f5aeb2f6dba4243a760588ed3dc6e1de23b4d752159b925032c872710528f135d5a466db373b0ce93d2c7e'
