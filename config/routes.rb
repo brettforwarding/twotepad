00c126b8e4451855efd033838b35bddcf847b793
Brett <brettforwarding@gmail.com>
2012-06-23 07:17:11 -0300

    Replicated Hartl static pages and their tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Twotepad::Application.routes.draw do
+
+  root to: 'static_pages#home'
+  
+  #match '/home',     to: 'static_pages#home'  # remove later
+  match '/help',     to: 'static_pages#help'
+  match '/about',    to: 'static_pages#about'
+  match '/contact',  to: 'static_pages#contact'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
